08dd29b2776571610e768a6b9f2dd025405f7cec
DarkDev <darkdev_brand@riseup.net>
2023-02-12 19:28:15 +0200

    add cyberway font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,4 +10,18 @@
 	:global(body) {
 		overflow-x: hidden;
 	}
+  :global(.text-cyberway) {
+		font-family: 'Cyberway';
+		line-height: 1.2;
+	}
+
+	@font-face {
+		font-family: 'Cyberway';
+		font-style: normal;
+		font-weight: 500;
+
+		src: local(''), /* Super Modern Browsers */ url('/fonts/cyberway.woff') format('woff'),
+			/* Modern Browsers */ url('/fonts/cyberway.ttf') format('truetype'),
+			/* Safari, Android, iOS */ url('/fonts/cyberway.svg') format('svg'); /* Legacy iOS */
+	}
 </style>
