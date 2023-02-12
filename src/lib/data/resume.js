19346a68ea4b1e22a4daa25ffe79fd4843b50250
DarkDev <darkdev_brand@riseup.net>
2023-02-12 19:26:26 +0200

    upd path for img`s

---
+++ src/lib/data/resume.js
@@ -0,0 +1,112 @@
+const resume = {
+	skillsList: [
+		{
+			img: '/images/resume/HTML5.svg',
+			text: 'HTML5',
+			link: 'https://en.wikipedia.org/wiki/HTML5'
+		},
+		{
+			img: '/images/resume/CSS3.svg',
+			text: 'CSS3',
+			link: 'https://en.wikipedia.org/wiki/CSS#CSS_3'
+		},
+		{
+			img: '/images/resume/js.svg',
+			text: 'Javascript',
+			link: 'https://en.wikipedia.org/wiki/JavaScript'
+		},
+		{
+			img: '/images/resume/nodeJs.svg',
+			text: 'NodeJS',
+			link: 'https://nodejs.org/en/'
+		},
+		{
+			img: '/images/resume/github.svg',
+			text: 'GITHUB',
+			link: 'https://github.com/'
+		},
+		{
+			img: '/images/resume/linux.svg',
+			text: 'Linux',
+			link: 'https://en.wikipedia.org/wiki/Linux'
+		},
+		{
+			img: '/images/resume/linux.svg',
+			text: 'Management',
+			link: ''
+		},
+
+		{
+			img: '/images/resume/design.svg',
+			text: 'Design',
+			link: ''
+		}
+	],
+	toolsList: [
+		{
+			img: '/images/resume/svelteJs.svg',
+			text: 'Svelte JS',
+			link: 'https://svelte.dev/'
+		},
+		{
+			img: '/images/resume/vueJs.svg',
+			text: 'Vue JS',
+			link: 'https://vuejs.org/'
+		},
+		{
+			img: '/images/resume/mongodb.svg',
+			text: 'Mongo DB',
+			link: 'https://www.mongodb.com/'
+		},
+		{
+			img: '',
+			text: 'TailwindCSS',
+			link: ''
+		},
+		{
+			img: '',
+			text: 'Mongo DB',
+			link: ''
+		},
+		{
+			img: '/images/resume/scrum.svg',
+			text: 'Scrum',
+			link: 'https://www.scrum.org/'
+		},
+		{
+			img: '/images/resume/m-3.svg',
+			text: 'M 3.0',
+			link: 'https://management30.com/'
+		},
+		{
+			img: '/images/resume/holacracy.svg',
+			text: 'Holacracy',
+			link: 'https://www.holacracy.org/'
+		},
+
+		{
+			img: '/images/resume/webpack.svg',
+			text: 'Webpack',
+			link: 'https://webpack.js.org/'
+		},
+
+		{
+			img: '/images/resume/figma.svg',
+			text: 'Figma',
+			link: 'https://www.figma.com/'
+		},
+		{
+			img: '/images/resume/XD.svg',
+			text: 'Adobe XD',
+			link: 'https://www.adobe.com/products/xd.html'
+		},
+		{
+			img: '/images/resume/illustrator.svg',
+			text: 'Illustrator',
+			link: 'https://www.adobe.com/products/illustrator/free-trial-download.html'
+		}
+	]
+};
+
+
+export default resume;
