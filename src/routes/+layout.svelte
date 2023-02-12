39f6e7967380fd477261a21af3c22ae9b4716dce
DarkDev <darkdev_brand@riseup.net>
2023-02-12 13:46:34 +0200

    clear

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,10 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
-	import Nav from '$lib/components/nav.svelte';
-	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import '../app.css';
-
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				enabled: browser
-			}
-		}
-	});
 </script>
 
-<QueryClientProvider client={queryClient}>
-	<div class="max-w-8xl mx-auto pt-4 pr-8 pb-8 pl-8">
-		<Nav />
-		<slot />
-	</div>
-</QueryClientProvider>
+<main class="">
+	<slot />
+</main>
 
 <style>
 	:global(body) {
