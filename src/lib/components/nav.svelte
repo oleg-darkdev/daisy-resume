9ce9b3dff8eeb5feed2bcf786de50dda18acd780
DarkDev <darkdev_brand@riseup.net>
2023-02-12 13:46:15 +0200

    alfa v.

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,43 +1,62 @@
-<script lang="ts">
-	import Themeswitcher from './themeswitcher.svelte';
+<script>
+  export let selectedSection, sections;
 </script>
 
-<div class="navbar bg-neutral shadow-md">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-			<!-- svelte-ignore a11y-label-has-associated-control -->
-			<label tabindex="0" class="btn btn-ghost btn-circle">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h16M4 18h7"
-					/></svg
-				>
-			</label>
-			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-			<ul
-				tabindex="0"
-				class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 font-poppins shadow"
-			>
-				<li><a href="/">Homepage</a></li>
-				<li><a href="/about">About</a></li>
-				<li><a href="/country">Country</a></li>
-			</ul>
-		</div>
-	</div>
-	<div class="navbar-center">
-		<!-- svelte-ignore a11y-missing-attribute -->
-		<a class="upper-case btn btn-ghost font-poppins text-xl text-primary">daisyUI</a>
-	</div>
-	<div class="navbar-end">
-		<Themeswitcher />
-	</div>
+<div class="flex flex-col w-full">
+<h2 class="text-5xl m-1 text-white">{selectedSection.text}</h2>
+<div class="w-full rounded-xl border p-1">
+  <div
+    on:click={() => {
+      selectedSection = sections[0];
+    }}
+    class="w-full rounded-t-xl h-16 bg-primary text-primary-content place-content-center hover:bg-white hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-primary text-2xl">Hello</h2>
+  </div>
+  <div
+    on:click={() => selectedSection = sections[1]}
+    class="w-full h-16 bg-secondary text-secondary-content place-content-center hover:bg-white hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-secondary text-2xl">About</h2>
+  </div>
+  <div
+    on:click={() => selectedSection = sections[2]}
+    class="w-full h-16 bg-success text-success-content place-content-center  hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-success text-2xl">Resume</h2>
+  </div>
+  <div
+    on:click={() => selectedSection = sections[3]}
+    class="w-full h-16 bg-warning text-warning-content place-content-center  hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-warning text-2xl">Portfolio</h2>
+  </div>
+    <div
+    on:click={() => selectedSection = sections[4]}
+
+    class="w-full   h-16 bg-info text-info-content place-content-center  hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-info text-2xl">Activism</h2>
+  </div>
+  <div
+    on:click={() => selectedSection = sections[5]}
+
+    class="w-full rounded-b-xl  h-16 bg-error text-error-content place-content-center hover:bg-gray-800"
+  >
+    <!-- <a href="#hello"></a> -->
+    <h2 class="h-full w-full text-center p-4 text-white hover:text-error text-2xl">Contact</h2>
+  </div>
+
 </div>
+</div>
+
+
+
+<style>
+
+</style>
