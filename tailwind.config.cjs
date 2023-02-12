0f983deacc065764750d0f36a252ef2fb87fca01
DarkDev <darkdev_brand@riseup.net>
2023-02-12 13:45:31 +0200

    upd theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,34 +10,21 @@ module.exports = {
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	// daisyUI config (optional)
-	daisyui: {
-		themes: [
-			{
-				dawn: {
-					primary: '#d7827e',
-					secondary: '#286983',
-					accent: '#b4637a',
-					neutral: '#f2e9e1',
-					'base-100': '#faf4ed',
-					info: '#56949f',
-					success: '#907aa9',
-					warning: '#ea9d34',
-					error: '#eb6f92'
-				}
-			},
-			{
-				rosepine: {
-					primary: '#ea9a97',
-					secondary: '#3e8fb0',
-					accent: '#eb6f92',
-					neutral: '#393552',
-					'base-100': '#191724',
-					info: '#9ccfd8',
-					success: '#c4a7e7',
-					warning: '#f6c177',
-					error: '#b4637a'
-				}
-			}
-		]
-	}
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#661AE6",
+          "secondary": "#D926AA",
+          "accent": "#1FB2A5",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272"
+        }
+      }
+    ]
+  }
 };
