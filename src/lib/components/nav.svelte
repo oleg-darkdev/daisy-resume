76616c54faf129810a6d08530e077374f40dd301
DarkDev <darkdev_brand@riseup.net>
2023-02-12 19:27:03 +0200

    upd links

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -1,62 +1,67 @@
 <script>
-  export let selectedSection, sections;
+	export let selectedSection, sections;
 </script>
 
-<div class="flex flex-col w-full">
-<h2 class="text-5xl m-1 text-white">{selectedSection.text}</h2>
-<div class="w-full rounded-xl border p-1">
-  <div
-    on:click={() => {
-      selectedSection = sections[0];
-    }}
-    class="w-full rounded-t-xl h-16 bg-primary text-primary-content place-content-center hover:bg-white hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-primary text-2xl">Hello</h2>
-  </div>
-  <div
-    on:click={() => selectedSection = sections[1]}
-    class="w-full h-16 bg-secondary text-secondary-content place-content-center hover:bg-white hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-secondary text-2xl">About</h2>
-  </div>
-  <div
-    on:click={() => selectedSection = sections[2]}
-    class="w-full h-16 bg-success text-success-content place-content-center  hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-success text-2xl">Resume</h2>
-  </div>
-  <div
-    on:click={() => selectedSection = sections[3]}
-    class="w-full h-16 bg-warning text-warning-content place-content-center  hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-warning text-2xl">Portfolio</h2>
-  </div>
-    <div
-    on:click={() => selectedSection = sections[4]}
-
-    class="w-full   h-16 bg-info text-info-content place-content-center  hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-info text-2xl">Activism</h2>
-  </div>
-  <div
-    on:click={() => selectedSection = sections[5]}
-
-    class="w-full rounded-b-xl  h-16 bg-error text-error-content place-content-center hover:bg-gray-800"
-  >
-    <!-- <a href="#hello"></a> -->
-    <h2 class="h-full w-full text-center p-4 text-white hover:text-error text-2xl">Contact</h2>
-  </div>
-
+<div class="flex w-full flex-col">
+	<div style="width: 300px;" class="wrap ">
+		<h2 class="m-1 text-5xl text-white">{selectedSection.text}</h2>
+		<div class="w-full rounded-xl border p-1">
+			<div
+				on:click={() => {
+					selectedSection = sections[0];
+				}}
+				class="h-16 w-full place-content-center rounded-t-xl bg-primary text-primary-content hover:bg-white hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-primary">Hello</h2>
+			</div>
+			<div
+				on:click={() => (selectedSection = sections[1])}
+				class="h-16 w-full place-content-center bg-secondary text-secondary-content hover:bg-white hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-secondary">
+					About
+				</h2>
+			</div>
+			<div
+				on:click={() => (selectedSection = sections[2])}
+				class="h-16 w-full place-content-center bg-success text-success-content  hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-success">Resume</h2>
+			</div>
+			<div
+				on:click={() => (selectedSection = sections[3])}
+				class="h-16 w-full place-content-center bg-warning text-warning-content  hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-warning">
+					Portfolio
+				</h2>
+			</div>
+			<div
+				on:click={() => (selectedSection = sections[4])}
+				class="h-16   w-full place-content-center bg-info text-info-content  hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-info">Activism</h2>
+			</div>
+			<div
+				on:click={() => (selectedSection = sections[5])}
+				class="h-16 w-full  place-content-center rounded-b-xl bg-error text-error-content hover:bg-gray-800"
+			>
+				<!-- <a href="#hello"></a> -->
+				<h2 class="h-full w-full p-4 text-center text-2xl text-white hover:text-error">Contact</h2>
+			</div>
+		</div>
+	</div>
 </div>
-</div>
-
-
 
 <style>
-
+	.wrap {
+		position: absolute;
+		right: 16%;
+		top: 20%;
+	}
 </style>
