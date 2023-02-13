9d2fceb9489d0c5445a3dbe6c69638c08118ca22
DarkDev <darkdev_brand@riseup.net>
2023-02-13 17:24:24 +0200

    add stats

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,5 +1,7 @@
 <script>
-  import SocialProjects from "./SocialProjects.svelte";
+	import SocialProjects from './SocialProjects.svelte';
+	import Statistics from './Statistics.svelte';
+	import statisticsData from '../data/statistics';
 	// добавить фото с хакатонов, выступлений и т.д.
 	const gallery = [
 		{
@@ -35,11 +37,13 @@
 	];
 </script>
 
+<Statistics statisticsData={statisticsData.personal} />
+
 <SocialProjects />
 
 <div class="mb-8 flex max-w-2xl flex-row flex-wrap">
 	{#each gallery as photo}
-		<div class="card card-compact m-1 w-80 bg-base-100 shadow-xl">
+		<div class="card-compact card m-1 w-80 bg-base-100 shadow-xl">
 			<figure><img src={photo.img} alt={photo.title} /></figure>
 			<div class="card-body">
 				<h2 class="card-title">{photo.title}</h2>
