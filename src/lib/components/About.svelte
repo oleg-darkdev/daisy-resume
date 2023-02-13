488b95a7bb99c0991c3dea8f514a53e41c8104dd
DarkDev <darkdev_brand@riseup.net>
2023-02-13 05:09:06 +0200

    alfa v.

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,50 @@
+<script>
+  import SocialProjects from "./SocialProjects.svelte";
+	// добавить фото с хакатонов, выступлений и т.д.
+	const gallery = [
+		{
+			img: '',
+			title: '',
+			desc: ''
+		},
+		{
+			img: '',
+			title: '',
+			desc: ''
+		},
+		{
+			img: '',
+			title: '',
+			desc: ''
+		},
+		{
+			img: '',
+			title: '',
+			desc: ''
+		},
+		{
+			img: '',
+			title: '',
+			desc: ''
+		},
+		{
+			img: '',
+			title: '',
+			desc: ''
+		}
+	];
+</script>
+
+<SocialProjects />
+
+<div class="mb-8 flex max-w-2xl flex-row flex-wrap">
+	{#each gallery as photo}
+		<div class="card card-compact m-1 w-80 bg-base-100 shadow-xl">
+			<figure><img src={photo.img} alt={photo.title} /></figure>
+			<div class="card-body">
+				<h2 class="card-title">{photo.title}</h2>
+				<p>{photo.desc}</p>
+			</div>
+		</div>
+	{/each}
+</div>
