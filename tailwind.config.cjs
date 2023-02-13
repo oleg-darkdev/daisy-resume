a9f512eab94fa76748b09e76e5942737e7cd0621
DarkDev <darkdev_brand@riseup.net>
2023-02-13 22:20:51 +0200

    upd color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,21 +10,22 @@ module.exports = {
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	// daisyUI config (optional)
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          "primary": "#661AE6",
-          "secondary": "#D926AA",
-          "accent": "#1FB2A5",
-          "neutral": "#191D24",
-          "base-100": "#2A303C",
-          "info": "#3ABFF8",
-          "success": "#36D399",
-          "warning": "#FBBD23",
-          "error": "#F87272"
-        }
-      }
-    ]
-  }
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#661AE6',
+					secondary: '#D926AA',
+					accent: '#1FB2A5',
+					neutral: '#191D24',
+					'base-100': '#2A303C',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272',
+
+				}
+			}
+		]
+	}
 };
