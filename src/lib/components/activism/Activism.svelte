74b87751b0d5b0a387eee0a07e6017ab21095829
DarkDev <darkdev_brand@riseup.net>
2023-02-13 22:19:54 +0200

    refactoring, add wrapper

--- src/lib/components/activism/Activism.svelte
+++ src/lib/components/activism/Activism.svelte
@@ -1,21 +1,27 @@
 <script>
-  import SocialProjects from "../SocialProjects.svelte";
-  import Statistics from '../Statistics.svelte';
-  import statisticsData from '../../data/statistics';
-  import activismGallery from '../../data/activism/gallery';
-  import Npm from './Npm.svelte';
+	import SocialProjects from './SocialProjects.svelte';
+	import Statistics from '../Statistics.svelte';
+	import statisticsData from '../../data/statistics';
+	import activismGallery from '../../data/activism/gallery';
+	import Npm from './Npm.svelte';
 </script>
 
 <Statistics statisticsData={statisticsData.social} />
-<SocialProjects />
-<Npm />
+<div class="info-dark mt-6 flex flex-col rounded-xl  p-4 md:w-10/12 lg:w-10/12 xl:w-10/12">
+	<SocialProjects />
+</div>
+<div class="info-dark mt-6 flex flex-col rounded-xl  p-4 md:w-10/12 lg:w-10/12 xl:w-10/12">
+	<Npm />
+</div>
 
-{#each activismGallery as photo}
-	<div style="max-height: 450px;" class="card mb-4 max-w-3xl bg-base-100 shadow-xl lg:card-side">
-		<figure><img async src={photo.img} alt="Sopot goverment" /></figure>
-		<div class="card-body">
-			<h2 class="card-title">{photo.title}</h2>
-			<p>{photo.desc}</p>
+<div class="info-dark mt-6 flex flex-col rounded-xl  p-4 md:w-10/12 lg:w-10/12 xl:w-10/12">
+	{#each activismGallery as photo}
+		<div style="max-height: 450px;" class="card mb-4 max-w-3xl bg-base-100 shadow-xl lg:card-side">
+			<figure><img async src={photo.img} alt="Sopot goverment" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">{photo.title}</h2>
+				<p>{photo.desc}</p>
+			</div>
 		</div>
-	</div>
-{/each}
+	{/each}
+</div>
