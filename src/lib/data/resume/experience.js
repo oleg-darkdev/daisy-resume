1f51481d72508bec145afc10b8725dfb3ec4b6a3
DarkDev <darkdev_brand@riseup.net>
2023-02-14 18:42:15 +0200

    add text data

--- src/lib/data/resume/experience.js
+++ src/lib/data/resume/experience.js
@@ -1,21 +1,229 @@
 const experienceData = [
+	// <a href="http://" target="_blank" class="link link-info" rel="noopener noreferrer"></a>
 	{
-		title: 'Lorem ipsum ',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-		stack: ['Design', 'SvelteJS', 'TailwindCSS'],
-		dates: ['11.10.2023', '11.20.2023']
+		title:
+			'UNICEF project  <a href="https://by-henry-ford.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer">Henry Ford`s Dream Job.</a>  .',
+		desc: [
+			'Responsible for the design and implementation of a training program for young people aged 16-18, utilizing technologies commonly used in the IT industry',
+			'Writted 25 educational lessons that covered the following topics: Git System, Scrum, Kanban Method, Agile methodology, Linux OS, and the role of the Product Owner.',
+			'Created a visually appealing graphic design  on Adobe Illustrator  and designed 7 educational boardgames with engaging gameplay mechanics.',
+			'Designed the user interface UI / UX for the 7 websites and 7 desktop applications for the boardgames, used SvelteJS, TailwindCSS, and NodeJS',
+			'Developed 7 desktop applications used SvelteJS, TailwindCSS, and NodeJS'
+		],
+		stack: ['Adobe Illustrator', 'SvelteJS', 'TailwindCSS', 'UI/UX'],
+		dates: ['02.02.2023', '30.09.2023 (present)'],
+		id: 1
 	},
 	{
-		title: '',
-		desc: '',
-		stack: ['', '', ''],
-		dates: ['', '']
+		title:
+			'StartUp: <a href="https://yellow-elephant.vercel.app/chess" target="_blank" class="link link-info" rel="noopener noreferrer">Yellow Elephant</a> ',
+		desc: [
+			'Designed the UI/UX for the web page and application of the Yellow Elephant, an organization that conducts chess tournaments based on the Swiss system.',
+			'Created the product graphical product design using Adobe Illustrator.',
+			'Developed the <a href="https://yellow-elephant.vercel.app/chess " target="_blank" class="link link-info" rel="noopener noreferrer">web page of the project,</a> and <a href="https://yellow-elephant.vercel.app/app" target="_blank" class="link link-info" rel="noopener noreferrer">the web application</a>, utilizing the following technologies: Google API Console, SvelteJS, NodeJS, TailwindCSS, and Microsoft Excel API.',
+			'Founded 1 private investor and 1 local sponsor to boost this project'
+		],
+		stack: ['SvelteJS', 'NodeJS', 'Google API Console', 'TailwindCSS'],
+		dates: ['05.01.2023', 'present'],
+		id: 2
 	},
 	{
-		title: '',
-		desc: '',
-		stack: ['', '', ''],
-		dates: ['', '']
+		title:
+			'The project <a href="https://emic.com.pl/" target="_blank" class="link link-info" rel="noopener noreferrer">"EduMigracje"</a>  ',
+		desc: [
+			'Created and managed an educational project called "EduMigracje" for the organization Emic',
+			'Designed the graphics for an educational board game using Adobe Illustrator and created the game mechanics for the game.',
+			`Designed the UI/UX for the project's website and the web-based application for the board game.`,
+			'Developed the <a href="https://by-pl.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer"> main page of the project</a>, and <a href="https://by-pl.vercel.app/app" target="_blank" class="link link-info" rel="noopener noreferrer">the web application</a>, using the SvelteJS, TailwindCSS, and NodeJS technologies.'
+		],
+		stack: ['Adobe Illustrator', 'TailwindCSS', 'SvelteJS'],
+		dates: ['21.12.2022', '30.01.2023'],
+		id: 3
+	},
+	{
+		title:
+			'StartUp: <a href="https://itingo.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer">ITingo platform</a>',
+		desc: [
+			'Led design and development of UI/UX for ITingo platform and 2 NGO websites using SvelteJS, TailwindCSS, Material Design system, and NodeJS',
+			'Created product graphic design for ITingo and various graphic materials for 2 NGO websites using Adobe Illustrator',
+			'Conducted needs analysis for 70 NGOs in the IT sector',
+			'Consulted with NGO representatives on the use of Kanban method and Scrum framework in their organizations',
+			'Recruited and managed on Agile methodology 7 IT volunteers in the project'
+		],
+		stack: ['SvelteJS', 'NodeJS', 'FlowbiteCSS', 'SocketIO', 'MongoDB'],
+		dates: ['01.10.2022', 'recent'],
+		id: 4
+	},
+	{
+		title:
+			'Project  <a href="https://krokpokroku.dlaodmiany.org.pl/" target="_blank" class="link link-info" rel="noopener noreferrer">"Odmienianie Krok po Kroku"</a>',
+		desc: [
+			'Completed graphic design using Adobe Illustrator and designed the gameplay mechanics for an educational board game about LGBTQIA+ flags and vexillology as part of the "Odmienianie Krok po Kroku" https://krokpokroku.dlaodmiany.org.pl/ project, funded by the "Fundusz dla Odmiany" foundation.',
+			'Led the collection and organization of information on LGBTQIA+ flag vexillology.',
+			'Developed the <a href="https://lgbt-tree.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer">web page of the project</a>  and a  <a href="https://lgbt-tree.vercel.app/app" target="_blank" class="link link-info" rel="noopener noreferrer">web-based application</a> for the board game utilizing technologies such as NFC, SvelteJS, TailwindCSS, and NodeJS.'
+		],
+		stack: ['Adobe Illustrator', 'SvelteJS', 'TailwindCSS'],
+		dates: ['22.10.2022', '15.12.2022'],
+		id: 5
+	},
+	{
+		title:
+			'Start Up: <a href="https://44hackerspace.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer">44FabLab</a>',
+		desc: [
+			'Established a partnership with the NGO "Alternatywy 5", allowing 44FabLab equipment to be installed at Alternatives 5 space',
+			'Improvement of sales and distribution of souvenirs produced by 44FabLab in souvenir stores with a potential annual turnover of $4000',
+			'3D modeling to customer, with subsequent printing on a 3D printer ',
+			'Creation of graphic design and sketches for laser cut souvenirs in Adobe Illustrator and Corel Draw',
+			'Organized 28 workshops in cooperation with "Alternatywy 5" for 6 months',
+			'Developed the <a href="https://44hackerspace.vercel.app/" target="_blank" class="link link-info" rel="noopener noreferrer">web portal</a> of the project  using SvelteJS, TailwindCSS, NodeJS and MongoDB',
+			'Repair and modernization of 3D printers',
+			`Designing UI/UX for the project's website`
+		],
+		stack: [
+			'SvelteJS',
+			'NodeJS',
+			'MongoDB',
+			'3D',
+			'FlowbiteCSS',
+			'Adobe Illustrator',
+			'UX/UI',
+			'Corel Draw'
+		],
+		dates: ['15.06.2022', 'present'],
+		id: 6
+	},
+	{
+		title: 'Freelance',
+		desc: [
+			'Developed web pages using technologies: Material Design System, Carbon Design System and developed 1 telegram bot using technologies NodeJS, TelegrafJS ',
+			'Created graphic designs in Adobe Illustrator',
+			'Creating apps for trello using Trello API, NodeJS, SvelteJS',
+			'Consulted on agile transformation and the use of Kanban for personal growth, integrated HyggerIO to management in NGO organizations ',
+			'Designing game mechanics for educational board games'
+		],
+		stack: [
+			'Adobe Illustrator',
+			'TailwindCSS',
+			'TelegrafJs',
+			'VueJs',
+			'Material Design System',
+			'Carbon Design System',
+			'Trello API',
+			'Scrum',
+			'Kanban'
+		],
+		dates: ['05.06.2022', '01.10.2022'],
+		id: 7
+	},
+	{
+		title:
+			'Project for <a href=" https://socialcamp.org/by" target="_blank" class="link link-info" rel="noopener noreferrer">SOCIAL CAMP foundation</a>',
+		desc: [
+			'Researched and organized information on repressions during the 2020 protests in Belarus',
+			'Designed graphics using Adobe Illustrator and developed gameplay mechanics for an educational board game',
+			'Developed project homepage and web application for the board game using SvelteJS, TailwindCSS, and NodeJS',
+			'Led a 2-person volunteer team using the Scrum framework'
+		],
+		stack: ['SvelteJS', 'FlowbiteCSS', 'NodeJS', 'Adobe Illustrator'],
+		dates: ['15.06.2021', '24.09.2021'],
+		id: 8
+	},
+	{
+		title: 'Start Up: RichBot',
+		desc: [
+			'Designed the product design in Adobe Illustrator',
+			'Implemented the UI/UX for the application using BootstrapCSS, Figma',
+			'Developed several modules for the automation of actions in crypto-project promotion using C# and ZennoPoster',
+			'Led a 4-persons team as a scrum master'
+		],
+		stack: ['BootstrapCSS', 'VueJs', 'C#', 'Cryptocurrencies', 'CSS3', 'Figma', 'Scrum', 'NodeJS'],
+		dates: ['17.10.2020', '20.06.2020'],
+		id: 9
+	},
+	{
+		title: 'Freelance',
+		desc: [
+			'Developed telegram 2 bots using NodeJS, C#, TelegrafJS technologies',
+			'Developed web pages using technologies: VueJs, TailwindCSS, Material Design System, Carbon Design System',
+			'Created graphic designs in Adobe Illustrator',
+			'Integrated technology SocketIO, FeatherJS, SvelteJs into existing projects',
+			'Designing game mechanics for educational board games'
+		],
+		stack: [
+			'VueJs',
+			'BootstrapCSS',
+			'NodeJS',
+			'TelegrafJs',
+			'HTML5',
+			'CSS3',
+			'Adobe Illustrator',
+			'SocketIO'
+		],
+		dates: ['02.03.2019', '02.12.2019'],
+		id: 10
+	},
+	{
+		title: 'StartUp: DarkDev.games',
+		desc: [
+			'Managed two teams of 5 people each, using Agile methodology and Scrum framework for 4 months',
+			'Organized a booth with educational board games in collaboration with event organizers at LVEE conference',
+			'Designed game mechanics for 17 educational board games',
+			'Created graphic designs in Adobe Illustrator for 12 educational board games',
+			'Developed UX/UI for 12 web applications complementing educational board games',
+			'Established a collaboration with a printing company "Listovka" for printing games with more favorable conditions',
+			'Developed 4 Telegram bots, using NodeJS, C#, TelegrafJS technologies and Development of 12 web applications using technologies: VueJs, SvelteJS, TailwindCSS, Material Design System, SocketIO'
+		],
+		stack: ['Material Design System', 'SvelteJS', 'NodeJS', 'Adobe Illustrator', 'TelegrafJs'],
+		dates: ['17.01.2019', 'present'],
+		id: 11
+	},
+	{
+		title:
+			'<a href="https://falanster.by/" target="_blank" class="link link-info" rel="noopener noreferrer">Falanster</a>',
+		desc: [
+			`Developed several critical components of the ecosystem, created a chat widget for the organization's homepage using stack: SocketIO, VueJs, HTML5, CSS3 `,
+			'Assisted in creating and mentoring a team of 6 IT volunteers.',
+			'Prepared a course with educational materials for volunteers',
+			'Developed UX/UI and product design for an IT volunteer project using Figma.',
+			'Assisted in setting up Ci/CD',
+			'Development of  several NPM packages for NodeJs'
+		],
+		stack: [
+			'HTML5',
+			'VueJs',
+			'BootstrapCSS',
+			'Linux',
+			'Adobe Illustrator',
+			'CSS3',
+			'Figma',
+			'SocketIO'
+		],
+		dates: ['10.09.2017', '10.08.2018'],
+		id: 12
+	},
+	{
+		title:
+			'<a href="https://swd.studio/" target="_blank" class="link link-info" rel="noopener noreferrer">Web studio: swd.studio</a> ',
+		desc: [
+			'Developed static web pages for clients using HTML5, CSS3, GulpJS, SCSS, and BootstrapCSS, resulting in improved user experience and increased client satisfaction.',
+			'Created branding images for web pages using Adobe Illustrator, resulting in enhanced visual appeal and improved brand recognition.',
+			`Designed UX/UI for clients' web pages using Figma, resulting in more user-friendly and visually appealing websites.`
+		],
+		stack: ['Adobe Illustrator', 'Figma', 'BootstrapCSS', 'CSS3', 'HTML5', 'GulpJS', 'SCSS'],
+		dates: ['10.09.2017', '10.05.2018'],
+		id: 13
+	},
+	{
+		title:
+			'<a href="https://integral.by/" target="_blank" class="link link-info" rel="noopener noreferrer">Integral</a> ',
+		desc: [
+			'Modernized industrial electrical equipment for microchip production as an electromechanical engineer',
+			'Programmed equipment using BASIC and Fortran programming languages',
+			'Performed verification and calibration functions as a process engineer'
+		],
+		stack: ['Fortran', 'BASIC', 'Electricity'],
+		dates: ['02.09.2011', '16.09.2017'],
+		id: 14
 	}
 ];
 
