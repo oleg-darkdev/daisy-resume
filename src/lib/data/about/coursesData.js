884d74a519a7f19d4fc3e7cb60f2d32981f2f309
DarkDev <darkdev_brand@riseup.net>
2023-02-14 21:16:46 +0200

    intial

---
+++ src/lib/data/about/coursesData.js
@@ -0,0 +1,96 @@
+const coursesData = [
+	{
+		title: 'Management 3.0: Management and Leadership',
+		desc: ['tools used in Management 3.0', '', '', ''],
+		institution: 'M3.0',
+		profession: 'Team Leader 3.0',
+		dates: ['02.04.2019', '06.04.2019'],
+		id: 1
+	},
+	{
+		title: 'KANBAN SYSTEM DESIGN - KMP1',
+		desc: ['kanban method', 'kanban system design', '', ''],
+		institution: 'Kanban University',
+		profession: 'Kanban master KMP-1',
+		dates: ['05.04.2019', '04.04.2019'],
+		id: 2
+	},
+	{
+		title: 'Mental Health and Psycho-Social Support',
+		desc: [
+			'The methodological course aims to present the basics psyhologies of working with humans, in particular working with children and students from Slavic countries.'
+			// 'The course will include a presentation of methods used in foreign language learning, as well as techniques of motivating and assessing.',
+			// 'The emotional regulation component will provide teachers with experties to support the functioning of refugee in a new environment and exceptionally difficult life circumstances.',
+		],
+		institution: 'SOK Foundation ',
+		profession: 'basics psyhologies of working with humans',
+		dates: ['26.10.2022', '12.11.2022'],
+		id: 8
+	},
+	{
+		title: 'Gdańsk Fundraising Academy',
+		desc: [
+			'NGO growth through non-grant funds',
+			'Building cooperation between NGOs and businesses',
+			'',
+			''
+		],
+		institution: 'Fundacja RC',
+		profession: 'The Fundraiser',
+		dates: ['04.10.2022', '15.01.2022'],
+		id: 10
+	},
+	{
+		title: 'Polish language course A2-B1',
+		desc: ['Learning Polish grammar', 'Practice with native speakers', 'Certificate Examination'],
+		institution: 'POLI training and integration center',
+		profession: 'Speaking Polish language',
+		dates: ['10.01.2021', '30.03.2021'],
+		id: 7
+	},
+	{
+		title: 'Polish language course B1',
+		desc: ['Learning Polish grammar', 'Practice with native speakers', 'Certificate Examination'],
+		institution: 'Caritas Gdansk',
+		profession: 'Speaking Polish language',
+		dates: ['20.10.2022', '08.01.2023'],
+		id: 9
+	},
+
+	{
+		title: 'Promocja projektów społecznych',
+		desc: ['Implementation and coordination of social initiatives', '', '', ''],
+		institution: 'Fundacja Inna Przestren',
+		profession: 'Social Initiatives Coordinator',
+		dates: ['03.02.2021', '27.02.2021'],
+		id: 6
+	},
+	{
+		title: 'Сampaigner and social activist',
+		desc: ['creation and implementation of petitions, changes in society', '', '', ''],
+		institution: 'Сampaigner school',
+		profession: 'Сampaigner',
+		dates: ['08.06.2019', '10.06.2019'],
+		id: 3
+	},
+	{
+		title: 'Youth Workshops',
+		desc: ['modern approaches and projects to work with young people', '', '', ''],
+		institution: 'Сivil forum',
+		profession: 'Working with young people',
+		dates: ['03.05.2019', '22.05.2019'],
+		id: 4
+	},
+	{
+		title: 'Social and grant projects',
+		desc: ['сreation and implementation of social projects', '', '', ''],
+		institution: 'CGI mogilev',
+		profession: 'Proffesional social activist',
+		dates: ['06.07.2019', '06.10.2019'],
+		id: 5
+	}
+];
+
+
+
+export default coursesData;
