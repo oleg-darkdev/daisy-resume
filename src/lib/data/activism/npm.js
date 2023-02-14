6f237601c78c280134322f684d68692bcd2c7f12
DarkDev <darkdev_brand@riseup.net>
2023-02-14 18:42:24 +0200

    add stats

--- src/lib/data/activism/npm.js
+++ src/lib/data/activism/npm.js
@@ -4,56 +4,64 @@ const npmPackages = [
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/cz-emoji-darkdev'
+		link: 'https://www.npmjs.com/package/cz-emoji-darkdev',
+		downloads: 6
 	},
 	{
 		title: 'node-cl-log',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
-		stack: 'Javascript',
+		stack: 'JS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/node-cl-log'
+		link: 'https://www.npmjs.com/package/node-cl-log',
+		downloads: 11
 	},
 	{
 		title: 'readme-bundler',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/readme-bundler'
+		link: 'https://www.npmjs.com/package/readme-bundler',
+		downloads: 3
 	},
 	{
 		title: 'node-m2h',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/node-m2h'
+		link: 'https://www.npmjs.com/package/node-m2h',
+		downloads: 4
 	},
 	{
 		title: 'node-md-bundler',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/node-md-bundler'
+		link: 'https://www.npmjs.com/package/node-md-bundler',
+		downloads: 4
 	},
 	{
 		title: 'node-sh-bundler',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/node-sh-bundler'
+		link: 'https://www.npmjs.com/package/node-sh-bundler',
+		downloads: 2
 	},
 	{
 		title: 'node-html-bundler',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/node-html-bundler'
+		link: 'https://www.npmjs.com/package/node-html-bundler',
+		downloads: 2
 	},
 	{
 		title: 'bad-data-generator',
 		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries',
 		stack: 'NodeJS',
 		img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.b3ru6LuO5h7f0TTu8xUuIgHaC4%26pid%3DApi&f=1&ipt=53dc75392444f9e06d4bea6e18592d1af0e7a092adca7361d46332528d040379&ipo=images',
-		link: 'https://www.npmjs.com/package/bad-data-generator'
+		link: 'https://www.npmjs.com/package/bad-data-generator',
+		downloads: 3
 	}
 ];
 
