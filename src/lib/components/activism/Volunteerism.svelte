1a9b456f2a859a985d3d73d0868b068fddef61a0
DarkDev <darkdev_brand@riseup.net>
2023-03-22 17:46:09 +0200

    change component card styles

--- src/lib/components/activism/Volunteerism.svelte
+++ src/lib/components/activism/Volunteerism.svelte
@@ -12,39 +12,37 @@
 	data-aos-mirror="true"
 	class="mb-2 rounded-xl bg-gray-800 p-4 text-5xl font-bold text-white md:w-8/12 lg:w-8/12 xl:w-8/12"
 >
-	My volunteerism
+	My story of volunteering
 </h2>
-<div
+
+<ul
 	data-aos="fade-up"
 	data-aos-delay="80"
 	data-aos-duration="800"
 	data-aos-once="true"
 	data-aos-mirror="true"
-	class="mb-6 flex flex-row flex-wrap "
+	class="steps steps-vertical "
 >
-	{#each !showFullVol ? volunteerism.slice(0, 2) : volunteerism as vol (vol.id)}
-		<div
-			class="card m-1 w-96 bg-gray-800 hover:bg-gray-900 {vol.showFullDesc
-				? 'h-auto'
-				: 'max-h-630'}  shadow-xl"
+	{#each volunteerism as vol (vol.id)}
+		<li
+			data-aos="fade-up"
+			data-aos-delay="80"
+			data-aos-duration="800"
+			data-aos-once="false"
+			data-aos-mirror="true"
+			class="step-info step "
 		>
-			<div class="card-body">
-				<h2 class="card-title text-gray-200">{@html vol.title}</h2>
-				<p class=" text-sm dark:text-gray-400">
-					{vol.dates[0]} - {vol.dates[1]}
-				</p>
-				{#each vol.showFullDesc ? vol.desc : vol.desc.slice(0, 2) as desc}
-					<p class="text-gray-200">{@html desc}</p>
-				{/each}
-				<button
-					class="btn-info btn-md btn mb-4 max-w-md"
-					on:click={() => (vol.showFullDesc = !vol.showFullDesc)}
-					>{!vol.showFullDesc ? 'Show' : 'Hide'} full description</button
-				>
+			<div class="card m-1 w-auto max-w-2xl bg-gray-800 shadow-xl hover:bg-gray-900">
+				<div class="card-body">
+					<h2 class="card-title text-gray-200">{@html vol.title}</h2>
+					<p class="text-start text-sm dark:text-gray-400">
+						{vol.dates[0]} - {vol.dates[1]}
+					</p>
+					{#each vol.desc as desc}
+						<p class="text-start text-gray-200">{@html desc}</p>
+					{/each}
+				</div>
 			</div>
-		</div>
+		</li>
 	{/each}
-</div>
-<button class="btn-info btn-lg btn w-full " on:click={() => (showFullVol = !showFullVol)}
-	>{!showFullVol ? 'Show' : 'Hide'} full volunteerism</button
->
+</ul>
