a8e510557ed56cc02dd6ff9fc3cde7acdc1ab69a
DarkDev <darkdev_brand@riseup.net>
2023-03-22 22:55:56 +0200

    refactoring photos component

--- src/lib/components/activism/Activism.svelte
+++ src/lib/components/activism/Activism.svelte
@@ -3,7 +3,8 @@
 	import Statistics from '../Statistics.svelte';
 	import Npm from './Npm.svelte';
 	import Volunteerism from './Volunteerism.svelte';
-
+  import Gallery from './Gallery.svelte';
+  
 	import statisticsData from '../../data/statistics';
 	import { gallery } from '../../data/activism/activismData';
 </script>
@@ -26,14 +27,6 @@
 	<SocialProjects />
 </div>
 
-<div class="mt-6 flex flex-col rounded-xl bg-gray-700  p-4 md:w-10/12 lg:w-10/12 xl:w-10/12">
-	{#each gallery as photo}
-		<div style="max-height: 450px;" class="p-12 card mb-4 max-w-3xl bg-base-100 shadow-xl lg:card-side">
-			<figure><img async src={photo.img} alt="Sopot goverment" /></figure>
-			<div class="card-body">
-				<h2 class="card-title text-gray-200">{photo.title}</h2>
-				<p class="text-gray-400">{photo.desc}</p>
-			</div>
-		</div>
-	{/each}
-</div>
+<!-- <div class="mt-6 flex flex-col rounded-xl bg-gray-700  p-4 md:w-10/12 lg:w-10/12 xl:w-10/12">
+  <Gallery {gallery} />
+</div> -->
---
+++ src/lib/components/activism/Gallery.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let gallery;
+</script>
+
+{#each gallery as photo}
+	<div
+		style="max-height: 450px;"
+		class="card mb-4 max-w-3xl bg-base-100 p-12 shadow-xl lg:card-side"
+	>
+		<figure><img async src={photo.img} alt="Sopot goverment" /></figure>
+		<div class="card-body">
+			<h2 class="card-title text-gray-200">{photo.title}</h2>
+			<p class="text-gray-400">{photo.desc}</p>
+		</div>
+	</div>
+{/each}
