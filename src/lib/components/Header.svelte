7747fc14e568cdbc18e832529bafe6758d760136
DarkDev <darkdev_brand@riseup.net>
2023-11-01 18:49:25 +0200

    add text logo, change nav panel style

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,77 +1,70 @@
 <script>
+	$: showNavPanel = false;
+
 	export let selectedSection, sections;
 </script>
 
 <!-- <header> -->
-	<header class="navbar bg-base-100">
-		<div class="navbar-start">
-			<div class="dropdown">
-				<label tabindex="0" class="btn-ghost btn-circle btn">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M4 6h16M4 12h16M4 18h7"
-						/></svg
-					>
-				</label>
-				<ul
-					tabindex="0"
-					class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+<header class="navbar bg-base-100">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<label tabindex="0"on:click={() => (showNavPanel = !showNavPanel)} class="btn-ghost btn-circle btn">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h7"
+					/></svg
 				>
+			</label>
+			{#if showNavPanel}
+				<ul tabindex="0" class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow">
 					<li
 						on:click={() => (selectedSection = sections[0])}
-						class="{selectedSection == sections[0]
-							? 'bg-gray-800 '
-							: 'bg-primary text-primary-content'} text-1xl h-10 p-1 text-white place-content-center rounded-t-xl"
+						class=" text-1xl h-16 place-content-center rounded-t-xl border-b border-gray-600 bg-gray-800 p-1 text-white"
 					>
 						Hello
 					</li>
 					<li
 						on:click={() => (selectedSection = sections[1])}
-						class="{selectedSection == sections[1]
-							? 'bg-gray-800 '
-							: 'bg-secondary text-secondary-content'} text-1xl h-10 text-white  p-1 place-content-center"
+						class=" text-1xl h-16 place-content-center  border-b border-gray-600 bg-gray-800  p-1 text-white"
 					>
 						About
 					</li>
 					<li
 						on:click={() => (selectedSection = sections[2])}
-						class="{selectedSection == sections[2]
-							? 'bg-gray-800 '
-							: 'bg-success text-success-content '} text-1xl h-10 text-white  p-1 place-content-center"
+						class=" text-1xl h-16 place-content-center  border-b border-gray-600 bg-gray-800  p-1 text-white"
 					>
 						Resume
 					</li>
 					<li
 						on:click={() => (selectedSection = sections[3])}
-						class="{selectedSection == sections[3]
-							? 'bg-gray-800 '
-							: 'bg-info text-info-content'} text-1xl h-10 p-1 text-white  place-content-center"
+						class=" text-1xl h-16 place-content-center  border-b border-gray-600 bg-gray-800  p-1 text-white"
 					>
 						Activism
 					</li>
 					<li
 						on:click={() => (selectedSection = sections[4])}
-						class="{selectedSection == sections[4]
-							? 'bg-gray-800 '
-							: 'bg-error text-error-content'} text-1xl h-10 p-1 text-white  place-content-center"
+						class=" text-1xl h-16 place-content-center  border-b border-gray-600 bg-gray-800 p-1  text-white"
 					>
 						Contact
 					</li>
 				</ul>
-			</div>
+			{/if}
 		</div>
-		<div class="navbar-center">
-			<a class="btn-ghost btn text-xl normal-case">daisyUI</a>
-		</div>
-		<!-- <div class="navbar-end">
+	</div>
+	<div class="navbar-center">
+		<!-- <a class=" text-xl normal-case"> -->
+			<img src="./images/darkdev-logo.svg" class="btn-ghost btn h-20 w-[240px]" alt="" />
+		<!-- </a> -->
+	</div>
+	<!-- <div class="navbar-end">
 
 			<button class="btn-ghost btn-circle btn">
 				<div class="indicator">
