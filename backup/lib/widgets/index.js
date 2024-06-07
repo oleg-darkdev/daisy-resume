3bb2bfd2ec48c5a5162ec021edadcbcb8c3917f9
DarkDev <darkdev_brand@riseup.net>
2024-06-07 05:17:35 +0300

    clear

--- backup/lib/widgets/index.js
+++
@@ -1,8 +0,0 @@
-import Hero from './Hero.svelte';
-import Footer from './Footer.svelte';
-import Aside from './Aside.svelte';
-import PdfViewer from './PdfViewer.svelte';
-
-
-
-export { Hero, Footer, Aside, PdfViewer };
