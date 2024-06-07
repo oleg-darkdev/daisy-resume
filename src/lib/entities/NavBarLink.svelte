07c59eaa8a769e6f6ddd03f1f8ce0237d432976d
DarkDev <darkdev_brand@riseup.net>
2024-06-07 06:34:04 +0300

    refactoring navigation panel

---
+++ src/lib/entities/NavBarLink.svelte
@@ -0,0 +1,23 @@
+<div id="nav-bar-link" class="into-view-1">
+  <a href="/about" class="navbar_link w-inline-block">
+    <div class="heading-style-h5 text-style-allcaps">About</div>
+    <div>
+      <div class="clip">
+        <div class="hover-arrow left">
+          <div class="icon-1x1-tiny w-embed">
+            <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13.1722 12L8.22217 7.04999L9.63617 5.63599L16.0002 12L9.63617 18.364L8.22217 16.95L13.1722 12Z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+        <div class="hover-arrow">
+          <div class="icon-1x1-tiny w-embed">
+            <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M13.1722 12L8.22217 7.04999L9.63617 5.63599L16.0002 12L9.63617 18.364L8.22217 16.95L13.1722 12Z" fill="currentColor"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  </a>
+</div>
--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -2,5 +2,6 @@ import ProgectCard from './ProgectCard.svelte';
 import ServiceCard from './ServiceCard.svelte';
 import FaqCard from './FaqCard.svelte';
 import CurverNav from './FaqCard.svelte';
+import NavBarLink from './FaqCard.svelte';
 
-export { ProgectCard, ServiceCard, FaqCard, CurverNav };
+export { ProgectCard, ServiceCard, FaqCard, CurverNav, NavBarLink };
