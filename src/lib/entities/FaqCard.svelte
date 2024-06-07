6961e3fdf8a59772bd495a3fec2d9fc25bd93a33
DarkDev <darkdev_brand@riseup.net>
2024-06-07 06:04:04 +0300

    alfa v.

---
+++ src/lib/entities/FaqCard.svelte
@@ -0,0 +1,32 @@
+<div data-w-id="635eb179-85f7-067d-94b1-dffe60beb2a9" class="home-faq-item no-border">
+    <div class="home-faq-top">
+    <div id="w-node-_635eb179-85f7-067d-94b1-dffe60beb2ab-60beb2a9" class="home-faq-number">
+        <div id="nowThis" thattextsplit="hoverTarget" base="20%" class="clip">
+        <div class="hover-text">
+            <div class="text-size-small text-style-muted">001</div>
+        </div>
+        <div class="hover-text bottom-hover-text">
+            <div class="text-size-small text-style-muted">001</div>
+        </div>
+        </div>
+    </div>
+    <div class="text-size-large">How long do projects typically take?</div>
+    <div class="home-faq-icon">
+        <div class="icon-1x1-small w-embed">
+        <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="currentColor"></path>
+        </svg>
+        </div>
+    </div>
+    </div>
+    <div class="hame-faq-bottom">
+    <div class="home-faq-content radius-regular">
+        <div class="home-faq-text">
+        <div class="text-size-regular text-color-white">This involves creating a custom solution for clients who want an online presence tailored to their specific needs. The process involves understanding the client's requirements, analyzing existing solutions, and then designing, coding, and testing the web application.</div>
+        </div>
+    </div>
+    </div>
+    <div class="line">
+    <div class="line-fill light-line-fill"></div>
+    </div>
+</div>
---
+++ src/lib/widgets/Feedback.svelte
@@ -0,0 +1,102 @@
+<div class="home-banner">
+                    <div class="padding-horizontal padding-small">
+                      <div class="home-banner_com">
+                        <div class="home-banner_bkg">
+                          <img src="" loading="lazy" alt="" class="image-fill">
+                          <div class="overlay light-overlay"></div>
+                        </div>
+                        <div class="home-banner_content slider-component">
+                          <div class="swiper">
+                            <div class="home-banner_slider swiper-wrapper">
+                              <div class="home-banner_slide swiper-slide">
+                                <div>
+                                  <div class="text-size-large text-align-center">Her custom website designs, coding solutions, branding strategies, graphic design and app design were all top notch.</div>
+                                </div>
+                                <div class="home-banner_credit">
+                                  <div class="home-banner_avatar">
+                                    <img src="" loading="lazy" alt="" class="image-fill">
+                                  </div>
+                                  <div>
+                                    <div class="text-size-small">Emily Humphry</div>
+                                    <div class="text-size-small text-style-muted">Airtable</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="home-banner_slide swiper-slide">
+                                <div>
+                                  <div class="text-size-large text-align-center">I highly recommend her to anyone looking for a creative freelancer who can provide comprehensive services tailored specifically to their needs!</div>
+                                </div>
+                                <div class="home-banner_credit">
+                                  <div class="home-banner_avatar">
+                                    <img src="" loading="lazy" alt="" class="image-fill">
+                                  </div>
+                                  <div>
+                                    <div class="text-size-small">Molly Wilson</div>
+                                    <div class="text-size-small text-style-muted">Facebook</div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="home-banner_slide swiper-slide">
+                                <div>
+                                  <div class="text-size-large text-align-center">She provided an incredible package of web design and development services that helped us increase our digital presence by 50%.</div>
+                                </div>
+                                <div class="home-banner_credit">
+                                  <div class="home-banner_avatar">
+                                    <img src="" loading="lazy" alt="" class="image-fill">
+                                  </div>
+                                  <div>
+                                    <div class="text-size-small">Karla Gordon</div>
+                                    <div class="text-size-small text-style-muted">Google</div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="home-banner_nav">
+                            <div id="w-node-a837a935-dca2-a76c-9b94-a9cb3179e530-019ada71" data-w-id="a837a935-dca2-a76c-9b94-a9cb3179e530" class="slider-arrow swiper-prev">
+                              <div class="clip">
+                                <div class="hover-arrow right">
+                                  <div class="icon-1x1-tiny w-embed">
+                                    <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M10.828 12L15.778 16.95L14.364 18.364L8 12L14.364 5.63599L15.778 7.04999L10.828 12Z" fill="currentColor"></path>
+                                    </svg>
+                                  </div>
+                                </div>
+                                <div class="hover-arrow">
+                                  <div class="icon-1x1-tiny w-embed">
+                                    <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M10.828 12L15.778 16.95L14.364 18.364L8 12L14.364 5.63599L15.778 7.04999L10.828 12Z" fill="currentColor"></path>
+                                    </svg>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="slider-arrow_text">
+                                <div class="text-size-small">prev</div>
+                              </div>
+                            </div>
+                            <div id="w-node-_8b9b0a96-ccd8-223c-687e-06fbe0610f98-019ada71" data-w-id="8b9b0a96-ccd8-223c-687e-06fbe0610f98" class="slider-arrow swiper-next">
+                              <div class="slider-arrow_text">
+                                <div class="text-size-small">next</div>
+                              </div>
+                              <div class="clip">
+                                <div class="hover-arrow left">
+                                  <div class="icon-1x1-tiny w-embed">
+                                    <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M13.1722 12L8.22217 7.04999L9.63617 5.63599L16.0002 12L9.63617 18.364L8.22217 16.95L13.1722 12Z" fill="currentColor"></path>
+                                    </svg>
+                                  </div>
+                                </div>
+                                <div class="hover-arrow">
+                                  <div class="icon-1x1-tiny w-embed">
+                                    <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                      <path d="M13.1722 12L8.22217 7.04999L9.63617 5.63599L16.0002 12L9.63617 18.364L8.22217 16.95L13.1722 12Z" fill="currentColor"></path>
+                                    </svg>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
