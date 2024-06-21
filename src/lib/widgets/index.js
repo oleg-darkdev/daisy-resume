628ee0a78a90fc005f1b11a1395e603f773cce11
DarkDev <darkdev_brand@riseup.net>
2024-06-22 02:31:25 +0300

    upd api

--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -5,7 +5,9 @@ import PdfViewer from './PdfViewer.svelte';
 import Feedback from './Feedback.svelte';
 import Header from './Header.svelte';
 import About from './Header.svelte';
+import BoardgamesList from './BoardgamesList.svelte';
+import Faq from './Faq.svelte';
 
 
 
-export { Hero, Footer, Aside, PdfViewer, Header, Feedback, About };
+export { Hero, Footer, Aside, PdfViewer, Header, Feedback, About, Faq, BoardgamesList };
