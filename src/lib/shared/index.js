f7affb6d1662c21953680a62a6f824e9a1addcbc
DarkDev <darkdev_brand@riseup.net>
2024-07-01 20:56:34 +0300

    upd api

--- src/lib/shared/index.js
+++ src/lib/shared/index.js
@@ -1,5 +1,3 @@
-// import HeadHrefLangs from './ui/HeadHrefLangs.svelte';
-import LocaleSwitcher from './ui/LocaleSwitcher.svelte';
 import {
 	Email,
 	PlayFilledAlt,
@@ -13,28 +11,68 @@ import {
 } from './ui/icons.js';
 
 import skillsTools from './data/skillsTools.js';
-import {portfolio, boardgamesProgects, ngoProgects, researchesProgects, historyProgects, footprintsFate, ecocide, antySystems, voteWise, byPl, lgbtTree, itingo,
-agileFans, bejahi, graffiti, darkDevFoundation, mks, historyOfAnarchy, ds5,
-yellowElephant, ddGames, techSopot
+
+import {
+	portfolio,
+	theStoriesBoardgames, 
+	agileFansBoardgames, 
+	antiSystemsBoardgames, 
+	otherBoardgames,
+	ngoProgects,
+	researchesProgects,
+	historyProgects,
+	footprintsFate,
+	ecocide,
+	antySystems,
+	voteWise,
+	byPl,
+	lgbtTree,
+	itingo,
+	agileFans,
+	bejahi,
+	graffiti,
+	darkDevFoundation,
+	mks,
+	historyOfAnarchy,
+	ds5,
+	yellowElephant,
+	ddGames,
+	techSopot
 } from './data/portfolio.js';
 
 // no mine
 import services from './data/services.js';
 
 export {
-	// replaceLocaleInUrl,
-  portfolio,
-  boardgamesProgects, ngoProgects, researchesProgects, historyProgects, footprintsFate, ecocide, antySystems, voteWise, byPl, lgbtTree, itingo,
-agileFans, bejahi, graffiti, darkDevFoundation, mks, historyOfAnarchy, ds5,
-yellowElephant, ddGames, techSopot,
-
-
-services,
+	portfolio,
+	theStoriesBoardgames, 
+	agileFansBoardgames, 
+	antiSystemsBoardgames, 
+	otherBoardgames,
+	ngoProgects,
+	researchesProgects,
+	historyProgects,
+	footprintsFate,
+	ecocide,
+	antySystems,
+	voteWise,
+	byPl,
+	lgbtTree,
+	itingo,
+	agileFans,
+	bejahi,
+	graffiti,
+	darkDevFoundation,
+	mks,
+	historyOfAnarchy,
+	ds5,
+	yellowElephant,
+	ddGames,
+	techSopot,
+	services,
 	skillsTools,
-	// HeadHrefLangs,
 	PlayFilledAlt,
 	PauseOutlineFilled,
-	LocaleSwitcher,
 	Email,
 	LogoLinkedin,
 	GameConsole,
--- src/lib/widgets/index.js
+++ src/lib/widgets/index.js
@@ -7,7 +7,9 @@ import Header from './Header.svelte';
 import About from './Header.svelte';
 import BoardgamesList from './BoardgamesList.svelte';
 import Faq from './Faq.svelte';
+import Services from './Services.svelte';
+import Gamification from './Gamification.svelte';
 
 
 
-export { Hero, Footer, Aside, PdfViewer, Header, Feedback, About, Faq, BoardgamesList };
+export { Hero, Footer, Aside, PdfViewer, Services, Gamification, Header, Feedback, About, Faq, BoardgamesList };
