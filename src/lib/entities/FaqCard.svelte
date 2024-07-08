91ed5ee87bcf5bd045955789a22a6ee4f2b0c966
DarkDev <darkdev_brand@riseup.net>
2024-07-08 11:11:23 +0300

    refactoring

--- src/lib/entities/FaqCard.svelte
+++ src/lib/entities/FaqCard.svelte
@@ -1,32 +1,51 @@
-<div data-w-id="635eb179-85f7-067d-94b1-dffe60beb2a9" class="home-faq-item no-border">
-    <div class="home-faq-top">
-    <div id="w-node-_635eb179-85f7-067d-94b1-dffe60beb2ab-60beb2a9" class="home-faq-number">
-        <div id="nowThis" thattextsplit="hoverTarget" base="20%" class="clip">
-        <div class="hover-text">
-            <div class="text-size-small text-style-muted">001</div>
-        </div>
-        <div class="hover-text bottom-hover-text">
-            <div class="text-size-small text-style-muted">001</div>
-        </div>
-        </div>
-    </div>
-    <div class="text-size-large">How long do projects typically take?</div>
-    <div class="home-faq-icon">
-        <div class="icon-1x1-small w-embed">
-        <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="currentColor"></path>
-        </svg>
+<script>
+    export let faqData;
+</script>
+
+<div class="home-faq-item no-border">
+    <div class="home-faq-top" on:click={()=> faqData.show = !faqData.show}>
+        <div class="faq-card home-faq-number">
+            <div id="nowThis" thattextsplit="hoverTarget" base="20%" class="clip">
+                <div class="hover-text">
+                    <div class="text-size-small text-style-muted">00{faqData.id}</div>
+                </div>
+                <div class="hover-text bottom-hover-text">
+                    <div class="text-size-small text-style-muted">00{faqData.id}</div>
+                </div>
         </div>
     </div>
+    <div class="text-size-large">{faqData.title}</div>
+        <div class="home-faq-icon">
+            <div class="icon-1x1-small w-embed">
+            <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z" fill="currentColor"></path>
+            </svg>
+            </div>
+        </div>
     </div>
+{#if faqData.show}
     <div class="hame-faq-bottom">
-    <div class="home-faq-content radius-regular">
-        <div class="home-faq-text">
-        <div class="text-size-regular text-color-white">This involves creating a custom solution for clients who want an online presence tailored to their specific needs. The process involves understanding the client's requirements, analyzing existing solutions, and then designing, coding, and testing the web application.</div>
+        <div class="home-faq-content radius-regular">
+            <div class="home-faq-text">
+            </div>
+            {#each faqData.desc as desc}
+                <p class='text-size-regular text-white mb-2'>
+                    {desc}
+                </p>
+            {/each}            
         </div>
     </div>
-    </div>
+{/if}
     <div class="line">
     <div class="line-fill light-line-fill"></div>
     </div>
 </div>
+
+
+<style>
+@media screen and (max-width: 767px) {
+.faq-card {
+    grid-area: span 1 / span 2 / span 1 / span 2;
+  }
+}
+</style>
--- src/lib/widgets/Faq.svelte
+++ src/lib/widgets/Faq.svelte
@@ -1,13 +1,22 @@
 <script>
 	import { FaqCard } from '$entities';
 	// import { portfolio, skillsTools } from '$shared';
+
+  const faq = [
+        {
+        id: '1',
+        title: `How long do projects typically take?`,
+        desc: [`This involves creating a custom solution for clients who want an online presence tailored to their specific needs. The process involves understanding the client's requirements, analyzing existing solutions, and then designing, coding, and testing the web application.`, ``]
+
+    }
+  ]
 </script>
 
 
 <div class="padding-horizontal padding-large">
   <div class="padding-bottom padding-large">
     <div class="home-service_grid">
-      <div class="padding-top">
+      <div class="">
         <div  class="font-optiker">
           QUESTIONS
           <!-- <div class="letter-animation">
@@ -26,10 +35,55 @@
         </div>
       </div>
       <div id="faq-wrap" class="home-faq-block">
+        <a name='faq' />
+
         <div class="home-faq-list radius-regular">
-            <FaqCard />
+          {#each faq as faqData}
+            <FaqCard {faqData}/>
+          {/each}
         </div>
       </div>
     </div>
   </div>
 </div>
+
+
+<style>
+.home-faq-block {
+  /* min-height: 35rem; */
+  grid-column-gap: 5rem;
+  grid-row-gap: 5rem;
+  flex-direction: column;
+  justify-content: space-between;
+  display: flex;
+}
+
+
+.home-service_grid {
+  grid-column-gap: 3rem;
+  grid-row-gap: 0rem;
+  flex-direction: column;
+  grid-template-rows: auto;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-columns: 1fr;
+  align-content: stretch;
+  justify-content: flex-start;
+  align-items: stretch;
+  margin-left: auto;
+  margin-right: auto;
+  /* padding-top: 6rem; */
+  display: flex;
+}
+
+
+@media screen and (max-width: 991px) {
+  .home-service_grid {
+    padding-top: 2rem;
+  }
+}
+@media screen and (max-width: 767px) {
+  .home-service_grid {
+    padding-top: 1rem;
+  }
+}
+</style>
