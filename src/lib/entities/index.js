8ebe5408966f46e29da00db051da31bd7dccb5a3
DarkDev <darkdev_brand@riseup.net>
2024-07-08 14:25:40 +0300

    upd api

--- src/lib/entities/index.js
+++ src/lib/entities/index.js
@@ -3,5 +3,8 @@ import ServiceCard from './ServiceCard.svelte';
 import FaqCard from './FaqCard.svelte';
 import CurverNav from './FaqCard.svelte';
 import NavBarLink from './FaqCard.svelte';
+import TimelineCard from './TimelineCard.svelte';
+// import NavBarLink from './FaqCard.svelte';
+// import NavBarLink from './FaqCard.svelte';
 
-export { ProgectCard, ServiceCard, FaqCard, CurverNav, NavBarLink };
+export { ProgectCard, ServiceCard, FaqCard, CurverNav, NavBarLink, TimelineCard };
