bfede840301fb4d914c1ffda935dba2b0b27171b
DarkDev <darkdev_brand@riseup.net>
2024-07-08 14:25:15 +0300

    upd structure sections, add anchors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Hero, Footer, BoardgamesList, Aside, Gallery, TextDivider, Feedback, Faq, Gamification, Header, Services, PdfViewer, About } from '$widgets';
+	import { Hero, Footer, BoardgamesList, Aside, Gallery, Timeline, MyStory, TextDivider, Feedback, Faq, Gamification, Header, Services, PdfViewer, About } from '$widgets';
 	import { ProgectCard,  CurverNav, NavBarLink } from '$entities';
 	import { portfolio, skillsTools, theStoriesBoardgames, agileFansBoardgames, antiSystemsBoardgames, otherBoardgames, } from '$shared';
 </script>
@@ -32,16 +32,25 @@
     <Services />
 
     <TextDivider title="Work" subTitle="Web Design, Web Development, Creative Development">
-        <img src="/images/screenshotes/voteWise/1.webp" loading="lazy" more="image" alt="" class="c-more-image">
+        <!-- <img src="/images/screenshotes/voteWise/1.webp" loading="lazy" more="image" alt="" class="c-more-image">
         <img src="/images/screenshotes/antiSystem/1.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-1">
         <img src="/images/screenshotes/graffityChronicles/2.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-2">
         <img src="/images/screenshotes/itingo/1.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-3">
         <img src="/images/screenshotes/footprintsFate/1.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-4">
-        <img src="/images/screenshotes/44games/1.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-5">
-    </TextDivider>
+        <img src="/images/screenshotes/44games/1.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-5"> -->
+        <img src="/gif/antiSystem.webp" loading="lazy" more="image" alt="" class="c-more-image">
+        <img src="/gif/darkdevFoundation.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-1">
+        <img src="/gif/graffityChronicles.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-2">
+        <img src="/gif/itingo.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-3">
+        <img src="/gif/lgbtqia.webp" loading="lazy" more="image" alt="" class="c-more-image bit-title-4">
+        <img src="/gif/antiSystem.webp " loading="lazy" more="image" alt="" class="c-more-image bit-title-5">
+
+      </TextDivider>
     
     <section class="home-about">
       <div class="padding-horizontal lg:padding-small md:padding-small xl:padding-small">
+         <a name="works" />
+         
         <div class="grid-large">
           <div id="servicet-wrap" class="padding-vertical padding-xlarge">
             <div id="home-work-wrap" class="home-work-block">
@@ -54,18 +63,14 @@
                 </div>
               </div> -->
 
-              
-
               <div class="home-work-bottom">
                 <div class="home-work-list w-dyn-list">
                   <div role="list" class="home-work-cms w-dyn-items">
-
-
-                    <a name="portfolio" />
+                   
                       {#each portfolio as portfolioData}
-                      <!-- year  -->
+
                       <div class="mx-auto ">
-                        <div class="mb-8 lg:mt-10 xl:mt-10 md:mt-10 mt-16 lg:ml-40 flex flex-shrink-0 flex-col  border-[#44149c] px-4 pb-6 lg:w-96 xl:w-96 md:w-96 w-64 border-b-4">
+                        <div class="mb-8 lg:mt-10 xl:mt-10 md:mt-10 mt-16 lg:ml-40 flex flex-shrink-0 flex-col  border-white px-4 pb-6 lg:w-96 xl:w-96 md:w-96 w-64 border-b-4">
                           <strong
                             class="text-thin heading-style-h3 flex text-left text-3xl font-thin leading-none  lg:text-4xl"
                           >
@@ -108,14 +113,14 @@
 
     <Gamification />
 
-    <TextDivider title="Games" subTitle="Gamification, Hybrid board games, Educational board games.">
+    <!-- <TextDivider title="Games" subTitle="Gamification, Hybrid board games, Educational board games.">
         <img src="/images/logos/boardgames/the-stories/bg.svg" loading="lazy" more="image" alt="" class="c-more-image">
         <img src="/images/logos/boardgames/anti-systems/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-1">
         <img src="/images/logos/boardgames/agile-fans/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-2">
-        <!-- <img src="/images/logos/boardgames/agile-fans/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-3">
+        <img src="/images/logos/boardgames/agile-fans/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-3">
         <img src="" loading="lazy" more="image" alt="" class="c-more-image bit-title-4">
-        <img src="" loading="lazy" more="image" alt="" class="c-more-image bit-title-5"> -->
-    </TextDivider>
+        <img src="" loading="lazy" more="image" alt="" class="c-more-image bit-title-5">
+    </TextDivider> -->
 
     <a name='boardgames' />
     <!-- <section class="home-about">
@@ -139,7 +144,7 @@
       </div>
     </section> -->
 
-    <TextDivider title="About" subTitle="Gamification, Hybrid board games, Educational board games.">
+    <TextDivider title="About" subTitle="Lorem Ipsum is simply dummy text of the printing and typesetting industry.">
         <!-- <img src="/images/logos/boardgames/the-stories/bg.svg" loading="lazy" more="image" alt="" class="c-more-image">
         <img src="/images/logos/boardgames/anti-systems/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-1">
         <img src="/images/logos/boardgames/agile-fans/bg.png" loading="lazy" more="image" alt="" class="c-more-image bit-title-2"> -->
@@ -148,9 +153,122 @@
         <img src="" loading="lazy" more="image" alt="" class="c-more-image bit-title-5"> -->
     </TextDivider>
 
-    <Gallery />
 
-    <section class="home-about">
+
+    <section class="home-about mt-20 mb-32">
+      <div class="padding-horizontal padding-small">
+        <div class="grid-large">
+          <div id="services" class="home-service-block services-wrap">
+            <div class="curve-block-center">
+              <a id="right" href="#my-story" class="curve-link w-inline-block">
+              <div class="curve-arrow">
+                  <div class="clip">
+                  <div class="hover-arrow top">
+                      <div class="icon w-embed">
+                      <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.0002 16.172L18.3642 10.808L19.7782 12.222L12.0002 20L4.22217 12.222L5.63617 10.808L11.0002 16.172V4H13.0002V16.172Z" fill="currentColor"></path>
+                      </svg>
+                      </div>
+                  </div>
+                  <div class="hover-arrow">
+                      <div class="icon w-embed">
+                      <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.0002 16.172L18.3642 10.808L19.7782 12.222L12.0002 20L4.22217 12.222L5.63617 10.808L11.0002 16.172V4H13.0002V16.172Z" fill="currentColor"></path>
+                      </svg>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+              <img src="/images/curver.svg" loading="lazy" alt="Curver img">
+              </a>
+            </div>
+
+            <a name="my-story" />
+            <div class="padding-top padding-huge">
+              <div class="padding-horizontal padding-medium">
+                <div class="home-service-top">
+                  <div class="home-service-intro">
+                    <div class="text-size-large text-align-center">
+                      With years of experience in the industry, I have worked with businesses of all sizes. <a href="">
+                    </div>
+                    <a href="#contact" class="button is-light w-inline-block">
+                      <div class="text-size-xsmall">Get in touch</div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="home-service">
+
+                <MyStory />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+    <Gallery />
+ 
+    
+<section class="home-about my-20">
+      <div class="padding-horizontal padding-small">
+        <div class="grid-large">
+          <div id="services" class="home-service-block services-wrap">
+            <div class="curve-block-center">
+              <a id="right" href="#services" class="curve-link w-inline-block">
+              <div class="curve-arrow">
+                  <div class="clip">
+                  <div class="hover-arrow top">
+                      <div class="icon w-embed">
+                      <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.0002 16.172L18.3642 10.808L19.7782 12.222L12.0002 20L4.22217 12.222L5.63617 10.808L11.0002 16.172V4H13.0002V16.172Z" fill="currentColor"></path>
+                      </svg>
+                      </div>
+                  </div>
+                  <div class="hover-arrow">
+                      <div class="icon w-embed">
+                      <svg width="420" height="420" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.0002 16.172L18.3642 10.808L19.7782 12.222L12.0002 20L4.22217 12.222L5.63617 10.808L11.0002 16.172V4H13.0002V16.172Z" fill="currentColor"></path>
+                      </svg>
+                      </div>
+                  </div>
+                  </div>
+              </div>
+              <img src="/images/curver.svg" loading="lazy" alt="Curver img">
+              </a>
+            </div>
+
+            <div class="padding-top padding-huge">
+              <div class="home-service">
+
+                <Timeline />
+
+            </div>
+
+             <div class="padding-top padding-huge">
+              <div class="padding-horizontal padding-medium">
+                <div class="home-service-top">
+                  <div class="home-service-intro">
+                    <div class="text-size-large text-align-center">If you have any questions, the F.A.Q. below should help you - or you can give them to me personally.</div>
+                    <a href="#contact" class="button is-light w-inline-block">
+                      <div class="text-size-xsmall">Get in touch</div>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="home-service">
+
+                <Faq />
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- <section class="home-about">
       <div class="padding-horizontal padding-small">
         <div class="grid-large">
           <div id="services" class="home-service-block services-wrap">
@@ -197,7 +315,7 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
   
 
     <Footer />
