5bf7f6f585098e6d10f0585357835a5c610f1cf8
DarkDev <darkdev_brand@riseup.net>
2024-07-08 11:11:32 +0300

    intial alfa v.

---
+++ src/lib/widgets/Gallery.svelte
@@ -0,0 +1,426 @@
+<section class="c-section  mb-20">
+<div class="c-container cc-ov">
+    <div class="c-hero-wrapper-about">
+    <div class="c-title-wrapper-2">
+        <h1 class="c-title-1_about text-white">Sometimes <br>
+        </h1>
+    </div>
+    <div class="c-title-wrapper-2">
+        <h1 class="c-title-1_about text-white">–Speaker <br>
+        </h1>
+    </div>
+    </div>
+    <div class="c-grid">
+    <div class="c-hero-wrapper-about is--mob">
+        <div class="c-hero-h1-wrapper">
+        <h1 class="c-title-1_about text-white">Sometimes  <br>
+        </h1>
+        </div>
+        <div class="c-hero-h1-wrapper">
+        <h1 class="c-title-1_about text-white">–Speaker <br>
+        </h1>
+        </div>
+    </div>
+    <div class="c-grid-item">
+        <img src="/images/speaker/speaker_1.jpg" loading="lazy" sizes="(max-width: 479px) 45vw, (max-width: 767px) 26vw, (max-width: 991px) 19vw, 15vw"  alt="" class="c-grid-item-img cc-one">
+        <img src="/images/speaker/speaker_6.png" loading="lazy" sizes="(max-width: 479px) 32vw, (max-width: 767px) 19vw, (max-width: 991px) 13vw, 11vw"  alt="" class="c-grid-item-img cc-two">
+        <img src="/images/speaker/speaker_3.jpg" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw"  alt="" class="c-grid-item-img cc-three">
+        <img src="/images/speaker/speaker_4.png" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw" alt="" class="c-grid-item-img cc-four">
+        <img src="/images/speaker/speaker_5.png" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw" alt="" class="c-grid-item-img cc-five">
+        <img src="/images/speaker/speaker_2.jpg" loading="lazy" sizes="(max-width: 479px) 45vw, (max-width: 767px) 26vw, (max-width: 991px) 19vw, 15vw"  alt="" class="c-grid-item-img cc-six">
+        <!-- <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw"  alt="" class="c-grid-item-img cc-seven">
+        <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw"  alt="" class="c-grid-item-img cc-eight">
+        <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 479px) 30vw, (max-width: 767px) 18vw, (max-width: 991px) 12vw, 10vw"  alt="" class="c-grid-item-img cc-nine">
+        <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 767px) 100vw, (max-width: 991px) 19vw, 15vw"  alt="" class="c-grid-item-img cc-ten">
+        <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 479px) 100vw, (max-width: 767px) 29vw, (max-width: 991px) 14vw, 9vw"  alt="" class="c-hero-img cc-one">
+        <img src="/images/bg-purple.gif" loading="lazy" sizes="(max-width: 767px) 100vw, (max-width: 991px) 14vw, 9vw" alt="" class="c-hero-img cc-two"> -->
+    </div>
+    </div>
+</div>
+</section>
+
+
+
+ <style>
+  @media screen and (max-width: 991px) {
+    .c-container {
+    padding-left: 18px;
+    padding-right: 18px;
+  }
+}
+
+  .c-container {
+  width: 100%;
+  max-width: 1440px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 2.5em;
+  padding-right: 2.5em;
+  display: block;
+}
+
+  .c-section {
+  /* padding-top: 8em;
+  padding-bottom: 8em; */
+  position: relative;
+  height: 120vh;
+}
+/* html {
+  -webkit-text-size-adjust: 100%;
+  -ms-text-size-adjust: 100%;
+  font-family: sans-serif;
+} */
+
+img {
+  border: 0;
+}
+
+img {
+  vertical-align: middle;
+  max-width: 100%;
+  display: inline-block;
+}
+h1 {
+  margin-bottom: 10px;
+  font-weight: 700;
+}
+h1 {
+  margin-top: 20px;
+  font-size: 38px;
+  line-height: 44px;
+}
+
+
+img {
+  max-width: 100%;
+  display: inline-block;
+}
+.c-grid {
+  grid-column-gap: 16px;
+  grid-row-gap: 16px;
+  grid-template-rows: auto auto;
+  grid-template-columns: 1fr 1fr;
+  grid-auto-columns: 1fr;
+  position: relative;
+}
+.c-hero-img {
+  position: absolute;
+}
+.c-hero-img.cc-one {
+  border-radius: 0.5rem;
+  width: 8.75rem;
+  height: 8.75rem;
+  top: auto;
+  bottom: -14.5rem;
+  left: 23.8rem;
+  right: auto;
+}
+.c-hero-img.cc-two {
+  border-radius: 0.5rem;
+  width: 8.75rem;
+  height: 8.75rem;
+  top: auto;
+  bottom: -66.4rem;
+  left: 50.5rem;
+  right: auto;
+}
+.c-grid-item-img {
+  border-radius: 0.5rem;
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+.c-grid-item-img.cc-one {
+  width: 13.75rem;
+  height: 9.375rem;
+  position: absolute;
+  top: -23rem;
+  left: 17.1rem;
+}
+.c-grid-item-img.cc-four {
+  border-radius: 0.5rem;
+  width: 9.375rem;
+  height: 9.375rem;
+  top: 9.6rem;
+  left: 4.7rem;
+}
+.c-grid-item-img.cc-three {
+  width: 9.375rem;
+  height: 9.375rem;
+  position: absolute;
+  top: -23rem;
+  left: auto;
+  right: 4.9rem;
+}
+.c-grid-item-img.cc-two {
+  border-radius: 0.5rem;
+  width: 10rem;
+  height: 10rem;
+  position: absolute;
+  top: -15.2rem;
+  left: 49.9rem;
+}
+.c-grid-item-img.cc-ten {
+  border-radius: 0.5rem;
+  width: 13.75rem;
+  height: 9.375rem;
+  top: 75.8rem;
+  left: 43.7rem;
+}
+.c-grid-item-img.cc-five {
+  object-fit: cover;
+  border-radius: 0.5rem;
+  width: 9.375rem;
+  height: 12.5rem;
+  top: 13.7rem;
+  left: 39.5rem;
+}
+.c-grid-item-img.cc-eight {
+  border-radius: 0.5rem;
+  width: 9.375rem;
+  height: 9.375rem;
+  top: 47rem;
+  bottom: auto;
+  left: auto;
+  right: 11.3rem;
+}
+.c-grid-item-img.cc-nine {
+  border-radius: 0.5rem;
+  width: 9.375rem;
+  height: 9.375rem;
+  top: 60.8rem;
+  left: 11.5rem;
+}
+.c-grid-item-img.cc-seven {
+  border-radius: 0.5rem;
+  width: 9.375rem;
+  height: 12.5rem;
+  top: 36.5rem;
+  left: 30.2rem;
+}
+.c-grid-item-img.cc-six {
+  border-radius: 0.5rem;
+  width: 13.75rem;
+  height: 9.375rem;
+  top: 2.5rem;
+  bottom: auto;
+  left: auto;
+  right: 0;
+}
+.c-grid-item {
+  position: relative;
+}
+.c-title-1_about {
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 11.25rem;
+  font-weight: 500;
+  line-height: 10.125rem;
+}
+.c-hero-wrapper-about {
+  z-index: 1;
+  position: relative;
+}
+.c-hero-wrapper-about.is--mob {
+  display: none;
+}
+.c-feature_faq-question:focus-visible {
+  outline-offset: -3px;
+  outline: 3px solid #004bb0;
+}
+@media screen and (max-width: 991px) {
+  
+  body {
+    font-size: 1rem;
+  }
+  .c-section {
+    padding-top: 60px;
+    padding-bottom: 60px;
+     height: 100vh;
+  }
+  .c-grid-item-img.cc-one {
+    width: 11.75rem;
+    height: 7.375rem;
+    top: -18.2rem;
+    left: 10.2rem;
+  }
+  .c-grid-item-img.cc-four {
+    width: 7.375rem;
+    height: 7.375rem;
+  }
+  .c-grid-item-img.cc-three {
+    width: 7.375rem;
+    height: 7.375rem;
+    top: -18.2rem;
+  }
+  .c-grid-item-img.cc-two {
+    width: 8rem;
+    height: 8rem;
+    top: -11.2rem;
+    left: 33.1rem;
+  }
+  .c-grid-item-img.cc-ten {
+    width: 11.75rem;
+    height: 7.375rem;
+  }
+  .c-grid-item-img.cc-five {
+    width: 7.375rem;
+    height: 10.5rem;
+  }
+  .c-grid-item-img.cc-eight,
+  .c-grid-item-img.cc-nine {
+    width: 7.375rem;
+    height: 7.375rem;
+  }
+  .c-grid-item-img.cc-seven {
+    width: 7.375rem;
+    height: 10.5rem;
+  }
+  .c-grid-item-img.cc-six {
+    width: 11.75rem;
+    height: 7.375rem;
+  }
+  .c-title-1_about {
+    font-size: 7.5rem;
+    line-height: 6.75rem;
+  }
+}
+@media screen and (max-width: 767px) {
+  .c-section {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+  .c-container {
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+  .c-hero-h1-wrapper {
+    text-align: center;
+  }
+  .c-hero-img.cc-two {
+    display: none;
+    bottom: -55.8rem;
+    left: 25.8rem;
+  }
+  .c-grid-item-img.cc-one {
+    width: 7.9375rem;
+    height: 5.375rem;
+    top: 7.625rem;
+    left: -2.8rem;
+  }
+  .c-grid-item-img.cc-four {
+    width: 5.375rem;
+    height: 5.375rem;
+    top: 18.8125rem;
+  }
+  .c-grid-item-img.cc-three {
+    width: 5.375rem;
+    height: 5.375rem;
+    top: 2.1875rem;
+    right: 6rem;
+  }
+  .c-grid-item-img.cc-two {
+    width: 5.75rem;
+    height: 5.75rem;
+    top: 13.0625rem;
+    left: 14.7rem;
+  }
+  .c-grid-item-img.cc-ten {
+    display: none;
+  }
+  .c-grid-item-img.cc-five {
+    width: 5.375rem;
+    height: 7.1875rem;
+  }
+  .c-grid-item-img.cc-eight {
+    width: 5.375rem;
+    height: 5.375rem;
+    top: 29.6rem;
+  }
+  .c-grid-item-img.cc-nine {
+    width: 5.375rem;
+    height: 5.375rem;
+    top: 40.4rem;
+    left: 4.5rem;
+  }
+  .c-grid-item-img.cc-seven {
+    width: 5.375rem;
+    height: 7.1875rem;
+    left: 26.8rem;
+  }
+  .c-grid-item-img.cc-six {
+    width: 7.9375rem;
+    height: 5.375rem;
+    top: 21.5625rem;
+  }
+  .c-title-1_about {
+    font-size: 4rem;
+    line-height: 4rem;
+  }
+}
+@media screen and (max-width: 479px) {
+  .c-section {
+    padding-top: 32px;
+    padding-bottom: 32px;
+  }
+  .c-container.cc-ov {
+    overflow: hidden;
+  }
+  .c-grid {
+    height: 58rem;
+  }
+  .c-hero-h1-wrapper {
+    text-align: center;
+  }
+  .c-hero-img.cc-one {
+    display: none;
+  }
+  .c-grid-item-img.cc-one {
+    width: 7.9375rem;
+    height: 5.375rem;
+  }
+  .c-grid-item-img.cc-two {
+    left: 19.7rem;
+  }
+  .c-grid-item-img.cc-ten {
+    width: 7.937rem;
+    height: 5.375rem;
+    display: none;
+    top: 54.375rem;
+    left: 23.9rem;
+  }
+  .c-grid-item-img.cc-five {
+    top: 37.1rem;
+    left: 2.5rem;
+  }
+  .c-grid-item-img.cc-eight {
+    top: 39.9rem;
+    right: -3rem;
+  }
+  .c-grid-item-img.cc-nine {
+    top: 45.4375rem;
+  }
+  .c-grid-item-img.cc-seven {
+    top: 32.2rem;
+    left: 12.1rem;
+  }
+  .c-grid-item-img.cc-six {
+    top: 22.9625rem;
+    right: -2rem;
+  }
+  .c-title-1_about {
+    font-size: 4rem;
+    line-height: 4rem;
+  }
+  .c-hero-wrapper-about {
+    display: none;
+  }
+  .c-hero-wrapper-about.is--mob {
+    display: block;
+    position: -webkit-sticky;
+    position: sticky;
+    top: 15%;
+  }
+}
+
+    </style>
