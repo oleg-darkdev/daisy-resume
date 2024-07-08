ce44556845a07433d416c5fcbae4e1dfcb8af715
DarkDev <darkdev_brand@riseup.net>
2024-07-08 14:24:52 +0300

    intial, alfa v.

---
+++ src/lib/widgets/Timeline.svelte
@@ -0,0 +1,42 @@
+<script>
+    import {TimelineCard} from '$entities';
+    import {timeline as timelineData} from '$shared';
+</script>
+
+<div class="padding-horizontal padding-large">
+<div class="padding-bottom padding-large">
+    <div class="">
+        <div class="about-history-block">
+        <div id="w-node-_44fe54da-fc95-84f6-1f71-7e4c772897d7-a49ada69" class="about-history-left">
+            <div class="about-history-image radius-regular">
+            <div class="rotate-card">
+                <div class="rotate-card-item">
+                <div class="rotate-card-image radius-regular">
+                    <div class="rotate-card-height"></div>
+                    <img src="images/speaker/speaker_1.jpg" loading="lazy" alt="Oleg Medvedev photo #2" class="hover:grayscale-0 hover:scale-110 hover:transition hover:duration-1000  hover:ease-in-out grayscale image-fill">
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div  class="about-history-list">
+            <div class="margin-bottom margin-large">
+                <div class="margin-bottom margin-small">
+                    <div class="title-tag">
+                    <div class="circle"></div>
+                    <div class="text-size-tiny text-style-allcaps">My timeline</div>
+                    </div>
+                </div>
+                <div class="about-history-top">
+                    <p class="text-size-xlarge">Over 4 years as a front-end developer</p>
+                </div>
+            </div>
+
+            {#each timelineData as timeline}
+                <TimelineCard {timeline}/>
+            {/each}
+        </div>
+        </div>
+    </div>
+    </div>    
+</div>
