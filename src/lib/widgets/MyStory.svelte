cd4742d5e2a4090f335d5361fefdcee7246605cf
DarkDev <darkdev_brand@riseup.net>
2024-07-08 14:24:40 +0300

    intial, alfa v.

---
+++ src/lib/widgets/MyStory.svelte
@@ -0,0 +1,68 @@
+
+
+<div  class="about-body-block bg-white radius-regular my-story-wrapper">
+    <div class="padding-horizontal padding-large">
+    <div class="">
+
+        <div class="font-optiker">
+            My Story
+        </div>
+    </div>
+    <div class="padding-bottom">
+        <div class="padding-top">
+        <div class="about-history-block">
+            <div  class="about-history-left">
+            <div  class="about-history-image radius-regular">
+                <div  class="rotate-card">
+                <div class="rotate-card-item">
+                    <div class="rotate-card-image radius-regular">
+                    <div class="rotate-card-height"></div>
+                    <img src="images/oleg_medvedev.png" loading="lazy" alt="Oleg Medvedev photo #1" class="grayscale hover:grayscale-0 hover:scale-110 hover:transition hover:duration-1000  hover:ease-in-out image-fill">
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+            <div  class="about-history-list">
+            <div class="about-story-right">
+                <div  class="margin-bottom margin-medium">
+                <div class="margin-bottom margin-small">
+                    <div class="title-tag">
+                    <div class="circle"></div>
+                    <div class="text-size-tiny text-style-allcaps">About me</div>
+                    </div>
+                </div>
+                <p class="text-size-xlarge">Engineer-IT enthusiast from #3Miasto</p>
+                </div>
+                <div  class="about-story-paragraph">
+                    <p class="text-size-regular text-color-grey mb-4">
+                        Digital maker, games designer, javascript programmer with 4 years of experience in board games development and 3 years of experience in web application development on various technologies in startups and opensource projects. 
+                    </p>
+
+                    <p class="text-size-regular text-color-grey mb-4">
+                        Evangelist svelteJS, have experience on VueJS and NodeJS, setup LinuxOS, AWS & BASH, rapid prototyping of interfaces using modern design systems and last UI trends: Material Design, Carbon Design, NeoBrutalism, Glassmorphism and Neumorphism
+                    </p>
+
+                    <p class="text-size-regular text-color-grey mb-4">
+                        Certified Specialist in Kanban Management (KMP-1), experience with scrum and Management3.0. and very extensive experience using various tools for communication, planning and prioritizing in team.
+                    </p>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+
+  
+
+
+
+  <style>
+.my-story-wrapper {
+  grid-area: 1 / 1 / 2 / 4;
+}
+  </style>
+
+      
