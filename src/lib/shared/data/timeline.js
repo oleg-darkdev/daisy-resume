0a71fc4b660c62d5e975f0064f2c284876a0bcc9
DarkDev <darkdev_brand@riseup.net>
2024-07-10 07:33:41 +0300

    upd content

--- src/lib/shared/data/timeline.js
+++ src/lib/shared/data/timeline.js
@@ -1,24 +1,40 @@
 const timeline = [
-    {
-        years: `2022-2024`,
-        title: `Freelance`,
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
-    },
-    {
-        years: `2020-2022`,
-        title: `VL - Web studio`,
-        desc: `And also banners, illustrations and other graphic elements aimed at improving visual perception and brand recognition.`
-    },
-    {
-        years: `2018-2019`,
-        title: `SWD - Web studio`,
-        desc: `Experience working in web studios specializing in the development of online storefronts and web plugins, creating a variety of graphic materials for web pages.`
-    },
-    {
-        years: '2018-2024',
-        title: 'IT volunteer and open-source contributor.',
-        desc: ``
-    },
-]
+  	{
+		years: `2023-2024`,
+		title: `44Games`,
+		desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
+	},
+
+	{
+		years: `2022-2023`,
+		title: `Freelance`,
+		desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`
+	},
+	{
+		years: `2020-2022`,
+		title: `VL - Web studio`,
+		desc: `And also banners, illustrations and other graphic elements aimed at improving visual perception and brand recognition.`
+	},
+	{
+		years: `2018-2019`,
+		title: `SWD - Web studio`,
+		desc: `Experience working in web studios specializing in the development of online storefronts and web plugins, creating a variety of graphic materials for web pages.`
+	},
+	{
+		years: '2018-2024',
+		title: 'IT volunteer and open-source contributor.',
+		desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	{
+		years: '2013-2017',
+		title: 'Integral',
+		desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+	},
+	// {
+	// 	years: '2018-2024',
+	// 	title: '',
+	// 	desc: ""
+	// },
+];
 
     export default timeline;
