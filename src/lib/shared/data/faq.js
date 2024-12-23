4d30cefedc6df3368319eaf1304edb83ed874da5
Bear-software <linner_952@mail.ru>
2024-12-23 17:12:24 +0100

    upd content

--- src/lib/shared/data/faq.js
+++ src/lib/shared/data/faq.js
@@ -1,53 +1,44 @@
 const faq = [
-    {
-        id: 1,
-        title: `How long do projects typically take?`,
-        desc: [
-            `This involves creating a custom solution for clients who want an online presence tailored to their specific needs. The process involves understanding the client's requirements, analyzing existing solutions, and then designing, coding, and testing the web application.`, 
-            ``
-        ]
-    },
-    {
-        id: 2,
-        title: `Lorem Ipsum is simply dummy text of the printing and?`,
-        desc: [
-            `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, 
-            ``
-        ]
-    },
-    {
-        id: 2,
-        title: `Lorem Ipsum is simply dummy text of the printing and?`,
-        desc: [
-            `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, 
-            ``
-        ]
-    },
-    {
-        id: 3,
-        title: `Lorem Ipsum is simply dummy text of the printing and?`,
-        desc: [
-            `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, 
-            ``
-        ]
-    },
-    {
-        id: 4,
-        title: `Lorem Ipsum is simply dummy text of the printing and?`,
-        desc: [
-            `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, 
-            ``
-        ]
-    },
-    {
-        id: 5,
-        title: `Lorem Ipsum is simply dummy text of the printing and?`,
-        desc: [
-            `Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`, 
-            ``
-        ]
-    },
-
+	{
+		id: 0,
+		title: `How long do projects typically take?`,
+		desc: [
+			`This involves creating a custom solution for clients who want an online presence tailored to their specific needs. The process involves understanding the client's requirements, analyzing existing solutions, and then designing, coding, and testing the web application.`,
+			``
+		]
+	},
+	{
+		id: 1,
+		title: 'Collaboration in grant or subsidized projects?',
+		desc: [
+			'I can assist you with the development and support of web applications, interactive platforms, and other solutions to ensure the successful implementation of socially significant projects.'
+		]
+	},
+	{
+		id: 2,
+		title: 'What types of projects are you interested in collaborating on?',
+		desc: [
+			'I am particularly interested in the following areas:',
+			'* Sports projects, especially those involving chess.',
+			'* Historical projects, such as those in the fields of archaeology, memorial studies, and cultural studies.',
+			'- Educational projects that use gamification to enhance learning.',
+			'- Digital projects that incorporate gamification elements for audience engagement.'
+		]
+	},
+	{
+		id: 3,
+		title: 'What does digital product support include?',
+		desc: [
+			'Support includes technical maintenance, feature updates, bug fixes, and product adaptation to user needs.'
+		]
+	},
+	{
+		id: 4,
+		title: 'What grant projects have you already implemented?',
+		desc: [
+			'My portfolio includes historical and cultural initiatives, as well as educational platforms with gamification. I am ready to share my experience and knowledge to help realize your projects.'
+		]
+	}
 ];
 
 
